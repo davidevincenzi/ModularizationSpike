b28f75b0179d3deb5bec80e466aa917f8e4a0220
Davide Vincenzi <davide.vincenzi@sherpany.com>
2019-12-18 16:31:18 +0100

    Added Sync module with Alamofire dependency

---
+++ Modules/Sync/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Sync",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Sync",
+            targets: ["Sync"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.9.1"),
+        .package(path: "../Persistence"),
+        .package(path: "../Models")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Sync",
+            dependencies: ["Persistence", "Models", "Alamofire"]),
+        .testTarget(
+            name: "SyncTests",
+            dependencies: ["Sync"]),
+    ]
+)
---
+++ Modules/Sync/Sources/Sync/Sync.swift
@@ -0,0 +1,5 @@
+import Alamofire
+
+struct Sync {
+    var text = "Hello, World!"
+}
