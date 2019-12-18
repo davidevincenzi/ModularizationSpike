d9df9e4de2f19a453824be1a47fefe3101dedda6
Davide Vincenzi <davide.vincenzi@sherpany.com>
2019-12-18 14:38:30 +0100

    Initial Commit

---
+++ ModularizationSpike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModularizationSpike
+//
+//  Created by Davide Vincenzi on 18.12.19.
+//  Copyright © 2019 Agilentia SA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
